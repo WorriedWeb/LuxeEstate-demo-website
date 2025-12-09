18ffc27f54e2cb3ddb87f7a4d131aad47a4d36b4
ankurkumar2002 <ankurkumarsingh0488@gmail.com>
2025-12-09 23:53:14 +0530

    added methods to allow in cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/luxeestate';
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type, Authorization"
+}));
 app.use(bodyParser.json({ limit: '10mb' })); // Increased limit for Base64 images
 
 // Database Connection
