94b8d75d5bc43f77b4552194793bd4cd3c324cbe
ankurkumar2002 <ankurkumarsingh0488@gmail.com>
2025-12-09 23:52:16 +0530

    made changes again in endpoints

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import serverless from "serverless-http";
 import app from "../server/index.js";
 
 export const config = {
-  runtime: "nodejs",
+  runtime: "nodejs18.x",
 };
 
 export default serverless(app);
